3edba0baba929e474beba923d3375efa5fd16205
Mihalachi Alexandrina <54944618+info11-maker@users.noreply.github.com>
2021-11-18 09:14:12 +0200

    Update app.js

--- src/assets/scss/js/app.js
+++ src/assets/scss/js/app.js
@@ -9,10 +9,27 @@ import 'what-input';
 window.jQuery = $;
 require('foundation-sites');
 
+$(document).ready(function(){
+
+    $(".menuIt").mouseover(function(){
+    var parent = this;
+        $(".boxes").fadeOut(500, function () {
+            switch ($(parent).attr("id")) {
+            case "1":
+                $("#box-1, #box-2, #box-3, #box-4, #box-5, #box-6, #box-7,#box-8, #box-9").fadeIn();
+                break;
+            case "2":
+                $("#box-10,#box-11,#box-12,#box-13, #box-14,#box-15,#box-16,#box-17").fadeIn();
+                break;
+            default:
+                break;
+        }
+        });
+    });
+  });
+
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
-
-
 $(document).foundation();
 
